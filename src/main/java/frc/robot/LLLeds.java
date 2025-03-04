a4c0ea62270916d7e26e2942d7d1ff2e55f108c5
Noskcaj <kcajdev@gmail.com>
2025-03-04 18:11:53 -0500

    Add limelight led controls

---
+++ src/main/java/frc/robot/LLLeds.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class LLLeds {
+    public static Command on(String limelight) {
+        return Commands.runOnce(() -> {
+            LimelightHelpers.setLEDMode_ForceOn(limelight);
+        });
+    }
+
+    public static Command off(String limelight) {
+        return Commands.runOnce(() -> {
+            LimelightHelpers.setLEDMode_ForceOff(limelight);
+        });
+    }
+
+    public static Command blink(String limelight) {
+        return Commands.runOnce(() -> {
+            LimelightHelpers.setLEDMode_ForceBlink(limelight);
+        });
+    }
+}
