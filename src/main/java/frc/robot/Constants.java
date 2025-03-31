65843aaf9a67d8afb7986a0a8f62921c0e77f9af
abigal3n <abbykittygaming13@gmail.com>
2025-03-31 16:52:06 -0400

    more reef tuning with better tuned elevator, it goes thonk

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -186,7 +186,7 @@ public final class Constants {
     public class ElevatorSetpoints {
       public static NTDouble l1 = new NTDouble(0, "l1", "ElevatorSetpoints");
       public static NTDouble l2 = new NTDouble(0.4, "l2", "ElevatorSetpoints");
-      public static NTDouble l3 = new NTDouble(0.77, "l3", "ElevatorSetpoints");
+      public static NTDouble l3 = new NTDouble(0.79, "l3", "ElevatorSetpoints");
       public static NTDouble l4 = new NTDouble(1.31, "l4", "ElevatorSetpoints");
 
       public static NTDouble groundLevel = new NTDouble(0, "groundLevel", "ElevatorSetpoints");
@@ -217,9 +217,9 @@ public final class Constants {
 
     public class StrafeOffsets {
       // static double l1 = 0;
-      public static NTDouble leftReef = new NTDouble(0.165, "leftReef", "StrafeOffsets");
+      public static NTDouble leftReef = new NTDouble(0.15, "leftReef", "StrafeOffsets");
       public static NTDouble leftL4 = new NTDouble(0.16, "leftL4", "StrafeOffsets");
-      public static NTDouble rightReef = new NTDouble(-0.15, "rightReef", "StrafeOffsets");
+      public static NTDouble rightReef = new NTDouble(-0.16, "rightReef", "StrafeOffsets");
       public static NTDouble rightL4 = new NTDouble(-0.18, "rightL4", "StrafeOffsets");
       public static NTDouble centerReef = new NTDouble(0.00, "centerReef", "StrafeOffsets");
       public static NTDouble processor = new NTDouble(-0.05, "processor", "StrafeOffsets");
